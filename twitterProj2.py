86d2aad3e472a409756494926f905c11669f425a
Brie <powell.brie2@gmail.com>
2022-09-29 04:42:16 -0400

    Testing some queries

--- twitterProj2.py
+++ twitterProj2.py
@@ -3,12 +3,12 @@ import nest_asyncio
 nest_asyncio.apply()
 
 c = twint.Config()
-hashtag = "#GoHomeMeghanMarkle"
+hashtag = "welcomehomeharryandmeghan"
 
 c.Search = hashtag
-c.Since = "2022-09-04"
-c.Until = "2022-09-20"
+c.Since = "2022-09-05"
+c.Until = "2022-09-28"
 c.Count = True
 c.User_full = True
-c.Output = "..\outputs\data.json"
+c.Output = ".\outputs\data_welcome01.json"
 twint.run.Search(c)
